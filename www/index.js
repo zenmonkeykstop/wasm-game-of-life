0a95d2eddd42a1e7cdc07c5ae950220550bb093e
Kevin O Gorman <kevin.ogorman@gmail.com>
2021-11-28 21:25:35 -0500

    added an fps counter

--- www/index.js
+++ www/index.js
@@ -82,6 +82,7 @@ const isPaused = () => {
 };
 
 const renderLoop = () => {
+    fps.render();
 
     drawGrid();
     drawCells();
@@ -161,5 +162,44 @@ stepButton.addEventListener("click", event => {
     drawGrid();
     drawCells();
 });
+
+const fps = new class {
+  constructor() {
+    this.fps = document.getElementById("fps");
+    this.frames = [];
+    this.lastFrameTimeStamp = performance.now();
+  }
+
+  render() {
+    // Convert the delta time since the last frame render into a measure
+    // of frames per second.
+    const now = performance.now();
+    const delta = now - this.lastFrameTimeStamp;
+    this.lastFrameTimeStamp = now;
+    const fps = 1 / delta * 1000;
+
+    // Save only the latest 100 timings.
+    this.frames.push(fps);
+    if (this.frames.length > 100) {
+      this.frames.shift();
+    }
+
+    // Find the max, min, and mean of our 100 latest timings.
+    let min = Infinity;
+    let max = -Infinity;
+    let sum = 0;
+    for (let i = 0; i < this.frames.length; i++) {
+      sum += this.frames[i];
+      min = Math.min(this.frames[i], min);
+      max = Math.max(this.frames[i], max);
+    }
+    let mean = sum / this.frames.length;
+
+    // Render the statistics.
+    this.fps.textContent = `
+FPS latest/avg/min/max: ${Math.round(fps)}/${Math.round(mean)}/${Math.round(min)}/${Math.round(max)}
+`.trim();
+  }
+};
 
 play();
